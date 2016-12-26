1c92d292ee1c18b6f767ac7761b4b31e2622aaee
kenjos75 <kenjos75@yahoo.com>
2016-12-26 23:46:39 +0800

    Modified ajax functionality.

--- ajax.php
+++ ajax.php
@@ -13,14 +13,36 @@
 		$score = (isset($_GET['score'])) ? $_GET['score'] : 0;
 		$name = (isset($_GET['name'])) ? $_GET['name'] : '';
 		if($score!='' && $name!=''){
-			if(!file_exists('datas/'.$name.'.json')){
-				$response['status'] = 'ok';
-				$createFile = fopen('datas/'.$name.'.json','w') or die("can't open file");
-				$createJson = json_encode(array('score'=>$score,'name'=>$name));
-				fwrite($createFile, $createJson);
-				fclose($createFile);
-			}
+			$response['status'] = 'ok';
 
+			if(file_exists('players.json')){
+				$readFile = fopen('players.json','w') or die("can't open file");
+				$contents = fread($readFile, filesize('players.json'));
+
+				$players = json_decode($contents);
+				$hasMatch = 0;
+				foreach ($players as $key => $value) {
+					if($value['name']==$name){
+						$hasMatch = 1;
+						$players[$key]['score'] = $score;
+					}
+				}
+
+				if($hasMatch==0){
+					array_push($players,array('score'=>$score,'name'=>$name));
+				}
+
+				$createJson = json_encode($players);
+				fwrite($readFile,$createJson);
+				fclose($readFile);
+
+			}else{
+				$createFile = fopen('players.json','w') or die("can't open file");
+				$createJson = json_encode(array(array('score'=>$score,'name'=>$name)));
+				fwrite($createFile, $createJson);
+				fclose($createFile);	
+			}
+			
 		}
 		echo json_encode($response);
 		
