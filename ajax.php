156b94c5ba0901c36d7b3de625801e59e97d6438
kenjos75 <kenjos75@yahoo.com>
2016-12-26 22:39:40 +0800

    Test ajax

--- ajax.php
+++ ajax.php
@@ -1,15 +1,30 @@
 <?php
 	
 
-	echo getcwd();
+	
 	$action = (isset($_GET['action'])) ? $_GET['action'] : '';
-
+	
 	if($action!=''){
 		$action();
 	}
 
 	function save_score(){
+		$response = array('status'=>'not ok');
+		$curDir = getcwd();
+		$score = (isset($_GET['score'])) ? $_GET['score'] : 0;
+		$name = (isset($_GET['name'])) ? $_GET['name'] : '';
+		if($score!='' && $name!=''){
 
+			if(!file_exists($curDir.'/data/'.$name.'.json')){
+
+				$createFile = fopen($curDir.'/data/'.$name.'.json','w');
+				$createJson = json_encode(array('score'=>$score,'name'=>$name));
+				fwrite($createFile, $createJson);
+				fclose($createFile);
+
+			}	
+		}
+		
 		//if(file_exists(filename))
 	}
 
